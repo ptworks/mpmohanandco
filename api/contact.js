da7f760f667afbd01f27107506e112796c07dcec
ptworks <tiru.prasad73@gmail.com>
2026-06-13 10:39:53 +0530

    nodemailer testing

--- api/contact.js
+++ api/contact.js
@@ -1,6 +1,17 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    success: true,
-    message: "API Working"
-  });
+import nodemailer from 'nodemailer';
+
+export default async function handler(req, res) {
+  try {
+    res.status(200).json({
+      success: true,
+      message: 'API Working'
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      error: error.message
+    });
+  }
 }
