0f3ca4cfccbd987e10d0c84c4a51aff36961ad7f
ptworks <tiru.prasad73@gmail.com>
2026-06-13 10:36:59 +0530

    contact debugging

--- api/contact.js
+++ api/contact.js
@@ -1,49 +1,6 @@
-const nodemailer = require('nodemailer');
-
-module.exports = async (req, res) => {
-  try {
-    const {
-      firstName,
-      email,
-      phone,
-      city,
-      service,
-      message
-    } = req.body;
-
-    const transporter = nodemailer.createTransport({
-      host: 'smtp.office365.com',
-      port: 587,
-      secure: false,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD
-      }
-    });
-
-    await transporter.sendMail({
-      from: process.env.SMTP_USER,
-      to: 'info@mpmohanandco.com',
-      replyTo: email,
-      subject: 'Website Enquiry',
-      html: `
-        <h2>New Enquiry</h2>
-        <p><b>Name:</b> ${firstName}</p>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Phone:</b> ${phone}</p>
-        <p><b>City:</b> ${city}</p>
-        <p><b>Service:</b> ${service}</p>
-        <p><b>Message:</b> ${message}</p>
-      `
-    });
-
-    return res.status(200).json({ success: true });
-
-  } catch (err) {
-    console.error(err);
-    return res.status(500).json({
-      success: false,
-      error: err.message
-    });
-  }
-};
+export default function handler(req, res) {
+  return res.status(200).json({
+    success: true,
+    message: "API Working"
+  });
+}
